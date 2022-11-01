db99883dac89d786032cc500d89ccec45272fbac
veniaventis <4aba3691@gmail.com>
2022-11-01 10:25:40 +0100

    new PR

--- src/main/java/api/ApiRequest.java
+++ src/main/java/api/ApiRequest.java
@@ -1,11 +1,10 @@
 package api;
 
 import api.models.*;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.SneakyThrows;
 import utlis.ApiUtils;
 import utlis.ConfigUtils;
-import utlis.JsonUtils;
-import utlis.UrlUtils;
 
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
@@ -18,52 +17,53 @@ public class ApiRequest {
     private static final String API_VERSION = ConfigUtils.getTestData("apiVersion");
 
     @SneakyThrows
-    public static SendPostModel sendPostOnTheWall(String message) {
-        String request = UrlUtils.sendPostUri(BASE_URL, Methods.SEND_POST.getMethod(), OWNER_ID, message, ACCESS_TOKEN, API_VERSION);
+    public static String sendPostOnTheWall(String message) {
+        String request = String.format("%s%s?owner_id=%s&message=%s&access_token=%s&v=%s", BASE_URL, Methods.SEND_POST.getMethod(), OWNER_ID, message, ACCESS_TOKEN, API_VERSION);
         ResponseJsonModel jsonResponse = ApiUtils.sendPostRequest(request);
-        return JsonUtils.jsonStringModelResponse(jsonResponse.getBody(), SendPostModel.class);
-
+        SendPostModel response = new ObjectMapper().readValue(jsonResponse.getBody().getObject().get("response").toString(), SendPostModel.class);
+        return response.getPostId();
     }
 
     public static String editPostWithAttachment(String message, String postId, String filePath) {
         String sentPhotoId = getSavedPhotoResponse(OWNER_ID, filePath).getId();
-        String request = UrlUtils.editPostWithAttachmentUri(BASE_URL, Methods.EDIT_POST.getMethod(), OWNER_ID, postId, message, sentPhotoId, ACCESS_TOKEN, API_VERSION);
+        String request = String.format("%s%s?owner_id=%s&post_id=%s&message=%s&attachments=photo%s_%s&access_token=%s&v=%s", BASE_URL, Methods.EDIT_POST.getMethod(), OWNER_ID, postId, message, OWNER_ID, sentPhotoId, ACCESS_TOKEN, API_VERSION);
         ResponseJsonModel jsonResponse = ApiUtils.sendPostRequest(request);
         return jsonResponse.getBody().getObject().get("response").toString();
     }
 
     @SneakyThrows
     public static SavePhotoModel getSavedPhotoResponse(String ownerId, String filePath) {
-        String request = UrlUtils.sendGetUrl(BASE_URL, Methods.PHOTO_UPLOAD_SERVER.getMethod(), ownerId, ACCESS_TOKEN, API_VERSION);
+        String request = String.format("%s%s?user_id=%s&access_token=%s&v=%s", BASE_URL, Methods.PHOTO_UPLOAD_SERVER.getMethod(), ownerId, ACCESS_TOKEN, API_VERSION);
         ResponseJsonModel jsonResponse = ApiUtils.sendGetRequest(request);
-        WallUploadServerModel wallUploadServerResponse = JsonUtils.jsonStringModelResponse(jsonResponse.getBody(),WallUploadServerModel.class);
+        WallUploadServerModel wallUploadServerResponse = new ObjectMapper().readValue(jsonResponse.getBody().getObject().get("response").toString(), WallUploadServerModel.class);
         ResponseJsonModel jsonResponse1 = ApiUtils.uploadResponse(wallUploadServerResponse.getUploadUrl(), filePath, "photo");
-        UploadModel uploadPhotoResponse = JsonUtils.jsonToStringModel(jsonResponse1.getBody(), UploadModel.class);
+        UploadModel uploadPhotoResponse = new ObjectMapper().readValue(jsonResponse1.getBody().toString(), UploadModel.class);
         String encodedURL = null;
         encodedURL = URLEncoder.encode(uploadPhotoResponse.getPhoto(), StandardCharsets.UTF_8);
-        String request1 = String.format("%s%s?user_id=%s&photo=%s&server=%s&hash=%s&access_token=%s&v=%s", BASE_URL, Methods.SAVE_WALL_PHOTO.getMethod(), ownerId, encodedURL, uploadPhotoResponse.getServer(), uploadPhotoResponse.getHash(), ACCESS_TOKEN, API_VERSION);
+        String request1 = String.format("%s%s?user_id=%s&photo=%s&server=%s&hash=%s&access_token=%s&v=%s", BASE_URL, Methods.SAVE_WALL_PHOTO.getMethod(),
+                ownerId, encodedURL, uploadPhotoResponse.getServer(), uploadPhotoResponse.getHash(), ACCESS_TOKEN, API_VERSION);
         ResponseJsonModel jsonResponse2 = ApiUtils.sendPostRequest(request1);
-        return JsonUtils.jsonToArray(jsonResponse2.getBody(), SavePhotoModel.class);
+        return new ObjectMapper().readValue(jsonResponse2.getBody().getObject().getJSONArray("response").get(0).toString(), SavePhotoModel.class);
     }
 
     @SneakyThrows
     public static String sendCommentToPost(String message, String postId) {
-        String request = UrlUtils.sendCommentUrl(BASE_URL, Methods.WALL_CREATE_COMMENT.getMethod(), OWNER_ID, postId, message, ACCESS_TOKEN, API_VERSION);
+        String request = String.format("%s%s?owner_id=%s&post_id=%s&message=%s&access_token=%s&v=%s", BASE_URL, Methods.WALL_CREATE_COMMENT.getMethod(), OWNER_ID, postId, message, ACCESS_TOKEN, API_VERSION);
         ResponseJsonModel jsonResponse = ApiUtils.sendPostRequest(request);
-        CommentModel response = JsonUtils.jsonStringModelResponse(jsonResponse.getBody(), CommentModel.class);
+        CommentModel response = new ObjectMapper().readValue(jsonResponse.getBody().getObject().get("response").toString(), CommentModel.class);
         return response.getCommentId();
     }
 
     @SneakyThrows
     public static boolean isLikedPost(String postId, String userId) {
-        String request = UrlUtils.isLikedUrl(BASE_URL, Methods.IS_LIKED.getMethod(), OWNER_ID, userId, postId, ACCESS_TOKEN, API_VERSION);
+        String request = String.format("%s%s?owner_id=%s&user_id=%s&type=post&item_id=%s&access_token=%s&v=%s", BASE_URL, Methods.IS_LIKED.getMethod(), OWNER_ID, userId, postId, ACCESS_TOKEN, API_VERSION);
         ResponseJsonModel jsonResponse = ApiUtils.sendGetRequest(request);
-        IsLikedModel response = JsonUtils.jsonStringModelResponse(jsonResponse.getBody(), IsLikedModel.class);
+        IsLikedModel response = new ObjectMapper().readValue(jsonResponse.getBody().getObject().get("response").toString(), IsLikedModel.class);
         return response.isLiked();
     }
 
     public static void deletePost(String postId) {
-        String request = UrlUtils.deletePostUrl(BASE_URL, Methods.DELETE_POST.getMethod(), OWNER_ID, postId, ACCESS_TOKEN, API_VERSION);
+        String request = String.format("%s%s?owner_id=%s&post_id=%s&access_token=%s&v=%s", BASE_URL, Methods.DELETE_POST.getMethod(), OWNER_ID, postId, ACCESS_TOKEN, API_VERSION);
         ApiUtils.sendPostRequest(request);
     }
 }
---
+++ src/main/java/utlis/ApiUtils.java
@@ -0,0 +1,44 @@
+package utlis;
+
+import api.models.ResponseJsonModel;
+import aquality.selenium.core.logging.Logger;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+
+import java.io.File;
+
+public class ApiUtils {
+
+    public static ResponseJsonModel sendGetRequest(String requestPath) {
+        HttpResponse<JsonNode> jsonResponse = null;
+        try {
+            jsonResponse = Unirest.get(requestPath).asJson();
+        } catch (UnirestException e) {
+            Logger.getInstance().debug("Get request doesn't send ", e);
+        }
+        return new ResponseJsonModel(jsonResponse.getStatus(), jsonResponse.getBody());
+    }
+
+    public static ResponseJsonModel sendPostRequest(String requestPath) {
+        HttpResponse<JsonNode> jsonResponse = null;
+        try {
+            jsonResponse = Unirest.post(requestPath)
+                    .asJson();
+        } catch (UnirestException e) {
+            Logger.getInstance().debug("New Post doesn't send", e);
+        }
+        return new ResponseJsonModel(jsonResponse.getStatus(), jsonResponse.getBody());
+    }
+
+    public static ResponseJsonModel uploadResponse(String httpBody, String filePath, String typeOfFile) {
+        HttpResponse<JsonNode> jsonResponse = null;
+        try {
+            jsonResponse = Unirest.post(httpBody).field(typeOfFile, new File(filePath)).asJson();
+        } catch (UnirestException e) {
+            Logger.getInstance().debug("Unable to upload file", e);
+        }
+        return new ResponseJsonModel(jsonResponse.getStatus(), jsonResponse.getBody());
+    }
+}
